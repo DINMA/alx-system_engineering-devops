30a47f433378ec736fb72d83abe07b385dd1c3f5
Agu Favour Chidinma <agufavourdinma@gmail.com>
2022-10-24 07:13:30 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-* void prototypes()
-* int prototypes()
-*/
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -17,5 +12,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
+int _putchar(char c);
 
 #endif /*MAIN_H*/
